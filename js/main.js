17e9456b14a24acfcb14937b6c8de64184dbb55a
wequbles <120826725+wequbles@users.noreply.github.com>
2024-08-03 03:03:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,17 +3,68 @@
 const varInput = document.getElementById('varInput');
 const varRange = document.getElementById('varRange');
 const paintedEl = document.getElementById('rainbow');
-const colors = ['#ff0000', '#ffdd00', '#00ff00', '#0000ff', '#8a2be2', '#ff00a6'];
+const contColors = document.getElementById('contColors');
+const arrColors = ['#ff0000', '#ffdd00', '#00ff00', '#0000ff', '#8a2be2', '#ff00a6'];
+const addColorBtn = document.getElementById('addColor');
 
-document.addEventListener('DOMContentLoaded', function() {
+function createColors(color, index, array) {
+	let colorBlock = document.createElement('div');
+	colorBlock.className = "color_block";
+
+	let newColor = document.createElement('input');
+	newColor.type = "color";
+	newColor.className = "color_input";
+	newColor.dataset.colorId = index;
+	newColor.dataset.delete = "false";
+	newColor.value = color;
+	colorBlock.append(newColor);
+
+	createDeletEl(colorBlock, index, array);
+
+	contColors.append(colorBlock);
+
+	array[index] = newColor.value;
+	newColor.addEventListener('input', () => {
+		array[index] = newColor.value; // update color
+	});
+
+	newColor.addEventListener('focus', () => {
+		newColor.dataset.delete = "true";
+	});
+
+	newColor.addEventListener('blur', () => {
+		newColor.dataset.delete = "false";
+	});
+}
+
+function createDeletEl(colorEl, colorId, arr) {
+	let deleteColorEl = document.createElement('button');
+	deleteColorEl.className = "color_delete";
+	deleteColorEl.dataset.colorId = colorId;
+	deleteColorEl.textContent = "Удалить";
+	colorEl.append(deleteColorEl);
+
+	deleteColorEl.addEventListener('click', () => {
+		arr.splice(colorId); // delete color
+		colorEl.remove();
+	});
+}
+
+addColorBtn.addEventListener('click', () => {
+	createColors('#000000', arrColors.length, arrColors);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	arrColors.forEach(createColors);
 	updateLabel();
 });
 
-varInput.addEventListener('change', function() {
+varInput.addEventListener('change', () => {
 	varRange.value = this.value;
+	updateLabel();
 });
 
-varRange.addEventListener('input', function() {
+varRange.addEventListener('input', () => {
 	updateLabel();
 });
 
@@ -29,14 +80,14 @@ function delay(ms) {
 	return new Promise((res) => setTimeout(res, ms));
 }
 
-async function PromiseTimeOut() {
-	for(let i = 0; i < colors.length; i++) {
+async function PromiseTimeOut(arr) {
+	for(let i = 0; i < arr.length; i++) {
 		let currentDelay = getDelay();
-		paintedEl.style.backgroundColor = colors[i];
+		paintedEl.style.backgroundColor = arr[i];
 		paintedEl.style.transition = `background-color ${currentDelay}ms linear`;
 		await delay(currentDelay);
 	}
-	PromiseTimeOut();
+	PromiseTimeOut(arrColors);
 }
 
-PromiseTimeOut();
+PromiseTimeOut(arrColors);
